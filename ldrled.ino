3bba5f56342bc8677b996e4af9c13dcef3cee4e3
Betül Koyuncu <82412848+betulkync@users.noreply.github.com>
2021-12-19 20:28:46 +0300

    Add files via upload

---
+++ ldrled.ino
@@ -0,0 +1,13 @@
+#define led 3
+void setup() {
+  pinMode(led,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int isik= analogRead(A0);
+  Serial.println(isik);
+  if(isik>900){digitalWrite(led,LOW);}
+  if(isik<850){digitalWrite(led,HIGH);}
+
+}
